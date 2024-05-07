5d8548424f801368b878d4de09e0c8d762674fb7
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-05-08 01:31:13 +0200

    save

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = process.env.isProduction === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
-  // target: isProduction ? 'server' : 'serverless',
 };
 
 export default nextConfig;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { useLang } from "@/context/Lang/LangContext";
+import Projects from "@/components/Projects/Projects";
 
 const Home: React.FC = () => {
   const { translations } = useLang();
   return (
     <main className="bg-body">
-      <p className={`text-text`}>{translations?.welcome}</p>
+      {/* <p className={`text-text`}>{translations?.welcome}</p> */}
+      <Projects />
     </main>
   );
 }
