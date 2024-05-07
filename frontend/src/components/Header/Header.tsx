ae5538f45685a20657b6d271a1ad8d166275098b
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-05-07 16:30:23 +0200

    first test depoyement

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -24,7 +24,7 @@ const Header: React.FC = () => {
           <div className="relative z-20 flex flex-col items-center justify-center">
               <div className="w-40 h-40 relative overflow-hidden">
                   <img className="w-full h-full object-cover rounded-full border-4 border-primary transform transition-transform duration-500 rotateY-0 hover:rotateY-180" src="/img/alex-pixel.png" alt="Description de l'image" />
-                  <img className="w-full h-full object-cover rounded-full border-4 border-primary transform transition-transform duration-500 rotateY-180 hover:rotateY-0" src="/img/autre-image.png" alt="Description de l'image" />
+                  {/* <img className="w-full h-full object-cover rounded-full border-4 border-primary transform transition-transform duration-500 rotateY-180 hover:rotateY-0" src="/img/autre-image.png" alt="Description de l'image" /> */}
               </div>
               <h1 className="text-4xl lg:text-6xl font-bold text-center text-primary relative hover:text-secondary">{translations.headerTitle}</h1>
           </div>
