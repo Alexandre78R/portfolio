225d5d9fbcbcb5f91864051e8aae7d1b8194bedd
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-05-08 02:08:00 +0200

    finish v1 skill

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { useLang } from "@/context/Lang/LangContext";
+import Skills from "@/components/Skills/Skills";
 
 const Home: React.FC = () => {
   const { translations } = useLang();
   return (
     <main className="bg-body">
-      <p className={`text-text`}>{translations?.welcome}</p>
+      {/* <p className={`text-text`}>{translations?.welcome}</p> */}
+      <Skills />
     </main>
   );
 }
