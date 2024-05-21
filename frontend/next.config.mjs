9b582f14ec7285e1bd755dbefc1eebc2d7ca578c
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-05-21 23:10:03 +0200

    sync

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,24 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  // webpackDevMiddleware: (config) => {
+  //   if (process.env.NEXT_DISABLE_HMR === 'true') {
+  //     config.watchOptions = {
+  //       ignored: /./,
+  //     };
+  //   }
+  //   return config;
+  // },
+  webpack: (config, { dev }) => {
+    // Désactiver le HMR en mode développement
+    if (dev) {
+      config.plugins = config.plugins.filter(
+        (plugin) => plugin.constructor.name !== 'HotModuleReplacementPlugin'
+      );
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
