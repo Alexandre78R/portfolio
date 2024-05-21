f42714b389d0494490e58648a0363b3941283502
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-05-22 00:20:15 +0200

    text fixed webpack

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,14 +10,25 @@ const nextConfig = {
   //   }
   //   return config;
   // },
-  webpack: (config, { dev }) => {
-    // Désactiver le HMR en mode développement
-    if (dev) {
+  // webpack: (config, { dev }) => {
+  //   // Désactiver le HMR en mode développement
+  //   if (dev) {
+  //     config.plugins = config.plugins.filter(
+  //       (plugin) => plugin.constructor.name !== 'HotModuleReplacementPlugin'
+  //     );
+  //   }
+
+  //   return config;
+  // },
+  webpack: (config, { isServer }) => {
+    // Si c'est le serveur, nous n'avons pas besoin de désactiver le HMR.
+    if (!isServer) {
+      // Filtrer les plugins webpack pour exclure le HotModuleReplacementPlugin.
       config.plugins = config.plugins.filter(
         (plugin) => plugin.constructor.name !== 'HotModuleReplacementPlugin'
       );
     }
-
+    
     return config;
   },
 };
