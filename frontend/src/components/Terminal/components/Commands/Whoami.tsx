fadf569281ea5d08c6e2f124f0235a3929945e12
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-06-03 01:05:46 +0200

    new css

--- frontend/src/components/Terminal/components/Commands/Whoami.tsx
+++ frontend/src/components/Terminal/components/Commands/Whoami.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import _ from "lodash";
 import { termContext } from "../../Terminal";
 import { Message } from "../TerminalStructure/Message";
+import Usage from "../TerminalStructure/Usage";
 
 const Whoami: React.FC = () => {
   const { arg } = useContext(termContext);
@@ -9,32 +10,36 @@ const Whoami: React.FC = () => {
   let message = "";
 
   if (arg.length === 0) {
-    message = "Merci d'indiquer un choix entre : experience|education|skills ";
-    return <Message>{message}</Message>;
+    message = "Merci d'indiquer un choix entre : experience | education | skills";
+    return (
+      <>
+        <Message>{message}</Message>
+        <Usage cmd="whoami"/>
+      </>
+    );
   }
 
   if (arg.length !== 1 && arg.length !== 0) {
-    message = "Merci de choisir 1 seul argument maximun entre : experience|education|skills";
+    message = "Merci de choisir 1 seul argument maximun entre : experience | education | skills";
     return <Message>{message}</Message>;
   }
 
 
   switch (arg[0].toLowerCase()) {
     case "experience":
-        message = 'experience'
-        break;
+      message = 'experience'
+      return <Message>{message}</Message>;
     case "education":
-        message = 'education'
-        break;
+      message = 'education'
+      return <Message>{message}</Message>;
     case "skills":
-        message = 'skills'
-        break;
+      message = 'skills'
+      return <Message>{message}</Message>;
     default:
-        message = "ce choix n'existe pas !"
-        break;
+      message = "ce choix n'existe pas !"
+      return <Message>{message}</Message>;
   }
 
-  return <Message>{message}</Message>;
 };
 
 export default Whoami;
--- frontend/src/components/Terminal/components/TerminalStructure/Usage.tsx
+++ frontend/src/components/Terminal/components/TerminalStructure/Usage.tsx
@@ -1,18 +1,32 @@
+import { useEffect } from "react";
 import { Message } from "./Message";
 
 type Props = {
-  cmd: "socials" | "themes";
+  cmd: "socials" | "themes" | "whoami";
 };
 
 const arg = {
-  socials: { placeholder: "social-no", example: "1" },
+  socials: { placeholder: "social", example: "1" },
   themes: { placeholder: "theme-name", example: "ubuntu" },
+  whoami: { placeholder: "whoami-view", example: "experience" },
 };
 
 const Usage: React.FC<Props> = ({ cmd }) => {
 
-  const action = cmd === "themes" ? "set" : "go";
+  let action = ""
 
+  if (cmd === "socials") {
+    action = "go";
+  }
+  
+  if (cmd === "themes") {
+    action = "set";
+  }
+
+  if (cmd === "whoami") {
+    action = "";
+  }
+  
   return (
     <Message data-testid={`${cmd}-invalid-arg`}>
       Usage: {cmd} {action} &#60;{arg[cmd]?.placeholder}&#62; <br />
