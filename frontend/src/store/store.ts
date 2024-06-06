3057fef17012416eac9c50960d7d1b9e7d614f18
Alexandre78R <alexandre.renard98@gmail.com>
2024-06-06 10:27:51 +0200

    delete test userSlice

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,18 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-
-// Importez vos slices ici
-import userReducer from './slices/userSlice';
 import skillsReducer from './slices/skillsSlice';
 
 const store = configureStore({
   reducer: {
-    // Ajoutez vos reducers ici
-    user: userReducer,
     skills: skillsReducer,
   },
 });
 
-// Types pour RootState et AppDispatch
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
