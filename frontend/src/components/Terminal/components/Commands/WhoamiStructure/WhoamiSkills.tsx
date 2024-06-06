05a4e00fcf85159c981891c6a21ca913dd17a97b
Alexandre78R <alexandre.renard98@gmail.com>
2024-06-06 11:27:35 +0200

    add reesponsive whoami skills

--- frontend/src/components/Terminal/components/Commands/WhoamiStructure/WhoamiSkills.tsx
+++ frontend/src/components/Terminal/components/Commands/WhoamiStructure/WhoamiSkills.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { termContext } from "../../../Terminal";
 import { Message } from "../../TerminalStructure/Message";
 import { useLang } from "@/context/Lang/LangContext";
@@ -6,9 +6,6 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 
 const WhoamiSkills: React.FC = () => {
-    const { arg } = useContext(termContext);
-    const { translations } = useLang();
-
     const dataSkills = useSelector((state: RootState) => state.skills.dataSkills);
 
     const chunkArray = (array: any[], size: number) => {
@@ -18,6 +15,21 @@ const WhoamiSkills: React.FC = () => {
         }
         return chunkedArr;
     };
+
+    const getChunkSize = () => {
+        if (window.innerWidth >= 1024) return 5; // Desktop
+        if (window.innerWidth >= 768) return 4; // Tablet
+        return 3; // Mobile
+    };
+
+    const [chunkSize, setChunkSize] = useState(getChunkSize());
+
+    useEffect(() => {
+        const handleResize = () => setChunkSize(getChunkSize());
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    
     return (
         <>
             {
@@ -25,7 +37,7 @@ const WhoamiSkills: React.FC = () => {
                     return (
                         <Message key={skill.id}>
                             <span className="text-primary">{`${skill.id}. ${skill.category}`}</span>
-                            {chunkArray(skill.skills, 3).map((skillChunk, index) => (
+                            {chunkArray(skill.skills, chunkSize).map((skillChunk, index) => (
                                 <div key={index} className="flex space-x-2 m-2">
                                     {skillChunk.map(skillImg => (
                                         <img key={skillImg.name} alt={skillImg.name} src={skillImg.image} />
