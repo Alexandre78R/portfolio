da9c478df113ccf70392ec642c3cd3b5660fdf71
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-06-10 11:52:21 +0200

    fixed responsive terminal

--- frontend/src/components/Terminal/HomeTerminal.tsx
+++ frontend/src/components/Terminal/HomeTerminal.tsx
@@ -1,9 +1,21 @@
 const HomeTerminal: React.FC = (): React.ReactElement => {
-    return (
-      <div className="inline-block mr-2">
-        <span className="text-secondary">visitor</span>@<span className="text-primary">alexandre-renard.dev</span> :~$
-      </div>
-    );
+
+  const textResponsive: (text: string) => string | undefined  = (text: string) : string | undefined => {
+    if (text == "visitor") {
+      if (window.innerWidth >= 350) return "visitor";  
+      return "vis."
+    }
+    if (text == "domaine") {
+      if (window.innerWidth >= 350) return "alexandre-renard.dev";  
+      return "a-renard.dev"
+    }
+  }
+
+  return (
+    <div className="inline-block mr-2">
+      <span className="text-secondary">{textResponsive("visitor")}</span>@<span className="text-primary">{textResponsive("domaine")}</span> :~$
+    </div>
+  );
 };
     
 export default HomeTerminal;
