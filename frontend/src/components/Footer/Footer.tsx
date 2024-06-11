d895b6ca2ed3839392c741dd6965e612771b216b
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-06-11 12:46:02 +0200

    add robot.txt and configure seo

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -10,13 +10,13 @@ const Footer: React.FC = (): React.ReactElement => {
     <footer className="bg-footer text-text py-4">
       <div className="container mx-auto flex flex-wrap justify-center">
         <div className="flex flex-col items-center mb-4 md:mb-0  lg:mx-15 flex-1">
-            <h4 className="text-lg font-bold mb-2">{translations.footerTitle}</h4>
+            <p className="text-lg font-bold mb-2">{translations.footerTitle}</p>
             <ul>
                 <li><a href="#" className="hover:text-secondary">{translations.footerAdmin}</a></li>
             </ul>
           </div>
           <div className="flex flex-col items-center mx-4 mb-4 md:mb-0 lg:mx-25 flex-1">
-            <h4 className="text-lg font-bold mb-2">{translations.footerNetworks}</h4>
+            <p className="text-lg font-bold mb-2">{translations.footerNetworks}</p>
             <div className='flex space-x-4'>
                 <a href="https://github.com/Alexandre78R" target='_blank' rel="alternate" title="Github">
                     <GitHubIcon className='text-text hover:text-secondary' /> 
@@ -27,8 +27,8 @@ const Footer: React.FC = (): React.ReactElement => {
             </div>
           </div>
           <div className="flex flex-col items-center mx-4 mb-4 md:mb-0 lg:mx-25 flex-1">
-          <h4 className="text-lg font-bold mb-2" >{translations.footerContact}</h4>
-                <p>contact@alexandre-renard.dev</p>
+            <p className="text-lg font-bold mb-2" >{translations.footerContact}</p>
+            <p>contact@alexandre-renard.dev</p>
           </div>
         </div>
         <div className="text-center mt-5">
