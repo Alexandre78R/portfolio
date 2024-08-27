be09cc083c781d1277bc9a5b967cd48e2d518a10
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-08-27 11:27:30 +0200

    fix tooglebutton

--- frontend/src/components/Button/ToggleButton.tsx
+++ frontend/src/components/Button/ToggleButton.tsx
@@ -11,8 +11,8 @@ const ToggleButton: React.FC<Props> = ({ toggleChecked, option1, option2, isChec
     <div className="relative inline-block" onClick={toggleChecked}>
       <label htmlFor="toggleButton" className="cursor-pointer">
         <div className="w-12 h-6 bg-gray-300 rounded-full shadow-inner">
-          <div className={`absolute left-0 top-1/2 transform -translate-y-1/2 z-20 w-6 text-xs font-bold text-center ${!isChecked ? 'text-white' : 'text-gray-500'}`}>{option1}</div>
-          <div className={`absolute right-0 top-1/2 transform -translate-y-1/2 z-20 w-6 text-xs font-bold text-center ${!isChecked ? 'text-gray-500' : 'text-white'}`}>{option2}</div>
+          <p className={`absolute left-0 top-1/2 transform -translate-y-1/2 z-20 w-6 text-xs font-bold text-center ${!isChecked ? 'text-white' : 'text-gray-500'}`}>{option1}</p>
+          <p className={`absolute right-0 top-1/2 transform -translate-y-1/2 z-20 w-6 text-xs font-bold text-center ${!isChecked ? 'text-gray-500' : 'text-white'}`}>{option2}</p>
           <div className={`absolute left-0 w-6 h-6 rounded-full bg-primary z-10 shadow-md transition-transform duration-300 ${isChecked ? 'transform translate-x-full' : ''}`}></div>
         </div>
       </label>
