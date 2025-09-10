4aeedbe268d368e759706c63bdadc21cb23d82d5
Alexandre78R <alexandre.renard98@gmail.com>
2025-09-10 14:24:10 +0200

    fixed test sendEmail

--- backend/tests/libs/generateSecurePassword.test.ts
+++ backend/tests/libs/generateSecurePassword.test.ts
@@ -56,7 +56,6 @@ describe('generateSecurePassword', () => {
             expect(hasCharFrom(password, numbers)).toBe(true);
             expect(hasCharFrom(password, symbols)).toBe(true);
 
-            // Vérifie seulement que tous les caractères sont imprimables et dans les sets connus
             for (const char of password) {
                 const isValidChar = uppercase.includes(char) || lowercase.includes(char) || numbers.includes(char) || symbols.includes(char);
                 expect(isValidChar).toBe(true);
