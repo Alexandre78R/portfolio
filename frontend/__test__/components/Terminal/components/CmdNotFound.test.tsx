5511ce321665514c35af88b07cb4c8759aecf8c6
Alexandre78R <alexandre.renard98@gmail.com>
2025-10-06 14:41:10 +0200

    add test CmdNotFound for Terminal

---
+++ frontend/__test__/components/Terminal/components/CmdNotFound.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { CmdNotFound } from "../../../../src/components/Terminal/components/CmdNotFound";
+import "@testing-library/jest-dom";
+
+jest.mock("../../../../src/components/Terminal/components/Message", () => ({
+  Message: ({ children }: any) => <div data-testid="message">{children}</div>,
+}));
+
+describe("CmdNotFound component", () => {
+  it("renders the correct message with the command", () => {
+    const cmdH: string = "foobar";
+    render(<CmdNotFound cmdH={cmdH} />);
+
+    const messageWrapper: HTMLElement = screen.getByTestId("message");
+    expect(messageWrapper).toBeInTheDocument();
+
+    expect(messageWrapper).toHaveTextContent(`Command not found: ${cmdH}`);
+  });
+});
