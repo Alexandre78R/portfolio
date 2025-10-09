de4b0ba4006660cf05144dd8643dfead5ff48558
Alexandre78R <alexandre.renard98@gmail.com>
2025-10-09 17:10:44 +0200

    add test whoamiCommand (Education)  for Terminal

---
+++ frontend/__test__/components/Terminal/components/Commands/WhoamiComponents/WhoamiEducation.test.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import WhoamiEducation from "@/components/Terminal/components/Commands/WhoamiComponents/WhoamiEducation";
+import { RootState } from "@/store/store";
+import { EducationType } from "@/store/slices/educationsSlice";
+
+const mockStore = configureStore([]);
+
+describe("WhoamiEducation Component", () => {
+  let store: ReturnType<typeof mockStore>;
+  
+  const mockEducations: EducationType[] = [
+    {
+        id: 1,
+        typeFR: "Éducation",
+        typeEN: "Education",
+        titleFR: "Master Informatique",
+        titleEN: "Master Computer Science",
+        title: "Master Informatique",
+        diplomaLevelFR: "Master",
+        diplomaLevelEN: "Master",
+        diplomaLevel: "Master",
+        school: "Université Paris",
+        location: "Paris, France",
+        year: 2024,
+        startDateFR: "Septembre 2023",
+        startDateEN: "September 2023",
+        startDate: "Septembre 2023",
+        endDateFR: "Juin 2024",
+        endDateEN: "June 2024",
+        endDate: "Juin 2024",
+        month: 9,
+    },
+  ];
+
+  it("should render without crashing when dataEducations is empty", () => {
+    store = mockStore({
+      educations: { dataEducations: [] },
+    } as Partial<RootState>);
+
+    render(
+      <Provider store={store}>
+        <WhoamiEducation />
+      </Provider>
+    );
+
+    // Vérifie qu'il n'y a pas d'éléments d'éducation
+    expect(screen.queryByText("Bachelor of Science")).not.toBeInTheDocument();
+  });
+
+  it("should render education items correctly", () => {
+    store = mockStore({
+      educations: { dataEducations: mockEducations },
+    } as Partial<RootState>);
+
+    render(
+      <Provider store={store}>
+        <WhoamiEducation />
+      </Provider>
+    );
+
+    // Vérifie que chaque titre, année, école et lieu sont présents
+    mockEducations.forEach((edu) => {
+      expect(screen.getByText(edu.year)).toBeInTheDocument();
+      expect(screen.getByText(edu.title)).toBeInTheDocument();
+      expect(screen.getByText(edu.school)).toBeInTheDocument();
+      expect(screen.getByText(`- ${edu.location}`)).toBeInTheDocument();
+    });
+  });
+});
