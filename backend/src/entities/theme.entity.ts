7fbc572bdc0b7a62b7687d84aa7c409d43bb5a75
Alexandre78R <alexandre.renard98@gmail.com>
2025-11-05 12:39:40 +0100

    Add entity  theme

---
+++ backend/src/entities/theme.entity.ts
@@ -0,0 +1,70 @@
+import { ObjectType, Field, ID } from "type-graphql";
+
+@ObjectType()
+export class Theme {
+  @Field(() => ID)
+  id: number;
+
+  @Field()
+  name: string;
+
+  // Base colors
+  @Field()
+  body: string;
+
+  @Field()
+  scrollHandle: string;
+
+  @Field()
+  scrollHandleHover: string;
+
+  @Field()
+  primary: string;
+
+  @Field()
+  secondary: string;
+
+  @Field()
+  success: string;
+
+  @Field()
+  error: string;
+
+  @Field()
+  warn: string;
+
+  @Field()
+  info: string;
+
+  @Field()
+  grey: string;
+
+  @Field()
+  placeholder: string;
+
+  @Field()
+  footer: string;
+
+  @Field()
+  admin: string;
+
+  // Text colors
+  @Field()
+  textDefault: string;
+
+  @Field()
+  text100: string;
+
+  @Field()
+  text200: string;
+
+  @Field()
+  text300: string;
+
+  @Field()
+  textButton: string;
+
+  // Visibility
+  @Field(() => Boolean)
+  visible: boolean;
+}
