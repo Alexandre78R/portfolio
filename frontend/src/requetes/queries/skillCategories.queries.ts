9c1264eba2a6a58ebbefc6d9999cb3adbdcf0482
Alexandre78R <alexandre.renard98@gmail.com>
2025-12-03 14:34:21 +0100

    fixed query skillcategory

--- frontend/src/requetes/queries/skillCategories.queries.ts
+++ frontend/src/requetes/queries/skillCategories.queries.ts
@@ -20,6 +20,26 @@ export const GET_SKILLS_LIST = gql`
   }
 `;
 
+export const SEARCH_SKILLS = gql`
+  query SearchSkills($searchTerm: String) {
+    searchSkills(searchTerm: $searchTerm) {
+      categories {
+        categoryFR
+        id
+        skills {
+          categoryId
+          id
+          image
+          name
+        }
+        categoryEN
+      }
+      code
+      message
+    }
+  }
+`;
+
 export const GET_SKILL_CATEGORY_BY_ID = gql`
   query GetSkillCategoryById($id: Int!) {
     skillCategoryById(id: $id) {
