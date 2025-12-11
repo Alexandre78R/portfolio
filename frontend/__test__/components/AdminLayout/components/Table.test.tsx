9a6e883178f0aeed935301b6e7f17d54bf325a5b
Alexandre78R <alexandre.renard98@gmail.com>
2025-12-11 16:18:22 +0100

    Fixed test components Table AdminLayout

--- frontend/__test__/components/AdminLayout/components/Table.test.tsx
+++ frontend/__test__/components/AdminLayout/components/Table.test.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+﻿import React from "react";
+import { render, screen } from '@test-utils';
 import Table, { ColumnDef, TableProps } from "../../../../src/components/AdminLayout/components/Table/Table";
 
 interface TestData {
