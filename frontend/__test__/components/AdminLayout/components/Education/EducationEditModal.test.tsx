0dc449f1305447cb0d72824741161ac95b95a494
Alexandre78R <alexandre.renard98@gmail.com>
2025-12-11 10:57:22 +0100

    Fixed test EducationEditModal

--- frontend/__test__/components/AdminLayout/components/Education/EducationEditModal.test.tsx
+++ frontend/__test__/components/AdminLayout/components/Education/EducationEditModal.test.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement } from "react";
-import { render, screen, fireEvent, waitFor, act } from "@testing-library/react";
+﻿import React, { ReactElement } from "react";
+import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
 import "@testing-library/jest-dom";
 
 import EducationEditModal from "@/components/AdminLayout/components/Education/EducationEditModal";
