518c1940ed63915e049bd65f956c128a5b1c26a9
Alexandre78R <alexandre.renard98@gmail.com>
2025-12-11 16:08:28 +0100

    Fixed test components DashboardCard AdminLayout

--- frontend/__test__/components/AdminLayout/components/ConfirmDialog.test.tsx
+++ frontend/__test__/components/AdminLayout/components/ConfirmDialog.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+﻿import React from 'react';
+import { render, screen, fireEvent } from '@test-utils';
 import ConfirmDialog, { ConfirmDialogProps } from '@/components/AdminLayout/components/ConfirmDialog/ConfirmDialog';
 
 jest.mock('@/components/ModalCustom/ModalCustom', () => {
