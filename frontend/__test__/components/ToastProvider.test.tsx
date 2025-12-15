fd3bafff78f1cb1769b37ac02a967c9dda3ab27e
Alexandre78R <alexandre.renard98@gmail.com>
2025-12-15 16:37:08 +0100

    Fixed test Toast alert

--- frontend/__test__/components/ToastProvider.test.tsx
+++ frontend/__test__/components/ToastProvider.test.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { render } from "@testing-library/react";
+﻿import React from "react";
+import { render } from '@test-utils';
 import ToastProvider from "@/components/ToastCustom/ToastProvider";
 
 describe("ToastProvider Component", (): void => {
