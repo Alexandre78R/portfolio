c2dc2b8d58c3ceb5a8a39a860856559f4f5cdb3c
Alexandre78R <alexandre.renard98@gmail.com>
2025-12-15 15:57:57 +0100

    Fixed test Loading

--- frontend/__test__/components/LoadingCustom.test.tsx
+++ frontend/__test__/components/LoadingCustom.test.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+﻿import React from "react";
+import { render, screen } from '@test-utils';
 import LoadingCustom from "@/components/Loading/LoadingCustom";
 import { CircularProgress as MuiCircularProgress, CircularProgressProps } from "@mui/material";
 
