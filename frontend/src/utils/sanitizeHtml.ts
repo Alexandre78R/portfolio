0c1267d384e47fa98d6a4ccdacaa2eb5541c0a42
Alexandre78R <alexandre.renard98@gmail.com>
2026-01-26 14:57:33 +0100

    add sanitizeHtml for DOMPurify - Security XSS Attacks for editor and preview signature

---
+++ frontend/src/utils/sanitizeHtml.ts
@@ -0,0 +1,21 @@
+import DOMPurify from 'dompurify';
+
+/**
+ * Sanitize HTML content to prevent XSS attacks
+ * @param dirty - Unsanitized HTML string
+ * @returns Sanitized HTML string safe to render
+ */
+export const sanitizeHtml: (dirty: string) => string = (dirty: string): string => {
+  return DOMPurify.sanitize(dirty, {
+    ALLOWED_TAGS: [
+      'p', 'br', 'strong', 'em', 'u', 's', 'a', 'ul', 'ol', 'li', 
+      'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'div', 
+      'span', 'pre', 'code', 'img', 'table', 'thead', 'tbody', 
+      'tr', 'th', 'td', 'hr'
+    ],
+    ALLOWED_ATTR: [
+      'href', 'target', 'rel', 'class', 'style', 'src', 'alt', 
+      'width', 'height', 'title'
+    ],
+  });
+};
